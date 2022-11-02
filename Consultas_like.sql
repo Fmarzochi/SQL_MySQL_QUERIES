91511c94ae817d9c34020f12f126607b45f51a3c
Felipe Marzochi <fmarzochi33@gmail.com>
2022-11-01 21:21:54 -0300

    addqueries

---
+++ Consultas_like.sql
@@ -0,0 +1,8 @@
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'
+AND EMBALAGEM = 'PET';
+
+SELECT * FROM tabela_de_clientes where nome like '%MATTOS';
+
+
